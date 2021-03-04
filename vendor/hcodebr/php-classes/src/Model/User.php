2e7a970083afc0f23aeb01e500c7e352d61ae58b
Rodrigo Moura <rodrigom.pe@outlook.com>
2021-03-04 19:50:37 -0300

    Esqueceu a Senha - Site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -10,6 +10,7 @@ class User extends Model {
 
 	const SESSION = "User";
 	const SECRET = "HcodePhp7_Secret";
+	const SECRET_IV = "HcodePhp7_Secret_IV";
 	const ERROR = "UserError";
 	const ERROR_REGISTER = "UserErrorRegister";
 
